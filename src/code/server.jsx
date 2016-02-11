cf64364c713d4fcd48cc2d48bad105c60cfa4697
unknown <kghadyani@TS001-EQLT045.amer.EPIQCORP.COM>
2016-02-11 15:21:16 -0600

    Fixed issue w/ charset not working because of JSX vs HTML

--- src/code/server.jsx
+++ src/code/server.jsx
@@ -22,7 +22,7 @@ function renderFullPage(renderedContent, initialState) {
 	return '<!doctype html>' + renderToStaticMarkup(
 		<html lang="en">
 		<head>
-			<meta charset="utf-8" />
+			<meta charSet="utf-8" />
 
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta name="author" content="Kevin Ghadyani" />
