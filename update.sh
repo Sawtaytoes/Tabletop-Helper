7afbe461bdbe0a534a3e0c2e23fbf1111a3cf08c
Kevin Ghadyani <Saturn2888@gmail.com>
2016-02-16 19:25:45 -0600

    Updated update scripts

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #/bin/bash
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH
+export NODE_ENV=production
+
+rm -r web/*
 git reset --hard HEAD
 git pull
 npm install
