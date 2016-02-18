60136a2c87b676af701f3b3665e849fd40da3c4d
Kevin Ghadyani <Saturn2888@gmail.com>
2016-02-18 01:47:09 -0600

    Brought in Header Nav actions from PulsenGame Website

--- src/code/actions/index.jsx
+++ src/code/actions/index.jsx
@@ -1,4 +1,52 @@
+// Page Meta
 export const UPDATE_PAGE_META = 'UPDATE_PAGE_META'
+
+export function updatePageMeta(path) {
+	return {
+		type: UPDATE_PAGE_META,
+		path
+	}
+}
+
+// Header Nav
+export const OPEN_MENU = 'OPEN_MENU'
+export const CLOSE_MENU = 'CLOSE_MENU'
+export const OPEN_SUBMENU = 'OPEN_SUBMENU'
+export const CLOSE_SUBMENU = 'CLOSE_SUBMENU'
+
+export function openMenu() {
+	return {
+		type: OPEN_MENU,
+		menuIsOpen: true
+	}
+}
+
+export function closeMenu() {
+	return {
+		type: CLOSE_MENU,
+		menuIsOpen: false,
+		submenuIsOpen: false
+	}
+}
+
+export function openSubMenu(submenuId) {
+	return {
+		type: OPEN_SUBMENU,
+		menuIsOpen: true,
+		submenuIsOpen: true,
+		submenuId
+	}
+}
+
+export function closeSubMenu() {
+	return {
+		type: CLOSE_SUBMENU,
+		submenuIsOpen: false,
+		submenuId: null
+	}
+}
+
+// Factions
 export const UPDATE_PLAYERS_COUNT = 'UPDATE_PLAYERS_COUNT'
 
 export const SELECT_FACTION = 'SELECT_FACTION'
@@ -9,14 +57,6 @@ export const DESELECT_SET_FACTIONS = 'DESELECT_SET_FACTIONS'
 
 export const SELECT_ALL_FACTIONS = 'SELECT_ALL_FACTIONS'
 export const DESELECT_ALL_FACTIONS = 'DESELECT_ALL_FACTIONS'
-
-// Page Meta
-export function updatePageMeta(path) {
-	return {
-		type: UPDATE_PAGE_META,
-		path
-	}
-}
 
 // Players
 export function updateNumberOfPlayers(numberOfPlayers) {
