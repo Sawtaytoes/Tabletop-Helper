c99e17c5f3f83b9865447d6e07d19def575a0c94
Kevin Ghadyani <Saturn2888@gmail.com>
2016-11-16 00:05:15 -0600

    Tweaked Home for a production-style deployment

--- src/code/content/nav-items.jsx
+++ src/code/content/nav-items.jsx
@@ -1,6 +1,6 @@
 module.exports = [{
 	name: 'Home',
-	to: 'home',
+	to: '',
 }, {
 	name: 'About',
 	to: 'about',
--- src/code/views/home.jsx
+++ src/code/views/home.jsx
@@ -10,14 +10,15 @@ const stylesLoader = StylesLoader.create()
 class Home extends PureComponent {
 	render() { return (
 		<div>
-			<h1>Hello World</h1>
+			<h1>Tabletop Helper</h1>
+			<h2>A small site of prototype tools from Kevin Ghadyani</h2>
 			<ul>
 				<li><Link to="/score-keeper" title="Score Keeper">Score Keeper</Link></li>
 				<li><Link to="/instructions" title="Instructions">Instructions</Link></li>
 				<li><Link to="/randomizer/smash-up" title="Smash-Up Randomizer">Smash-Up Randomizer</Link></li>
 			</ul>
 
-			<Link to="/about" title="Go to About">About</Link>
+			{/*<Link to="/about" title="Go to About">About</Link>*/}
 		</div>
 	)}
 }
