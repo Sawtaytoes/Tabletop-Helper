af4f2be9f805259fc1952f0d0b5075450d75b94c
Kevin Ghadyani <Saturn2888@gmail.com>
2017-03-19 10:31:37 -0500

    Added some rounting around Smash Up randomizer rewrite

--- src/views/randomizer-smash-up2.jsx
+++ src/views/randomizer-smash-up2.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route, Redirect, Link } from 'react-router-dom'
 
 // Components
 import PlayersSelector from 'components/players-selector'
@@ -6,8 +7,27 @@ import SmashUpDecksSelector from 'components/smash-up-decks-selector'
 
 const RandomizerSmashUp = () => (
 	<div>
-		<PlayersSelector />
-		<SmashUpDecksSelector />
+		<Redirect from="/randomizer/smash-up" to="/randomizer/smash-up/players-selector" />
+
+		<Route path="/randomizer/smash-up/players-selector" render={() => (
+			<div>
+				<div><Link to="/randomizer/smash-up/deck-selector">Next</Link></div>
+				<PlayersSelector />
+				<div><Link to="/randomizer/smash-up/deck-selector">Next</Link></div>
+			</div>
+		)} />
+
+		<Route path="/randomizer/smash-up/deck-selector" render={() => (
+			<div>
+				<div><Link to="/randomizer/smash-up/players-selector">Next</Link></div>
+				<SmashUpDecksSelector />
+				<div><Link to="/randomizer/smash-up/players-selector">Next</Link></div>
+			</div>
+		)} />
+
+
+		{/*<Link to="/randomizer/smash-up/" />*/}
+
 	</div>
 )
 
